c85b886bb0f962fb86e5b99770e99f0271629fb3
Alex Riley <ajcriley@gmail.com>
2020-10-24 22:45:22 +0100

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="transfinite",
-    version="0.2.0",
+    version="0.3.0",
     description="Transfinite ordinals for Python",
     author="Alex Riley",
     license="MIT",
